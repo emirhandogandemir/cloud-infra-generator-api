e444939b13178141c904a70f40279a337b24882b
Emirhan Dogandemir <emirhandgndmr51@gmail.com>
2023-05-21 15:37:14 +0300

    delete and create feature has been added to DatabaseAzure

--- controllers/databaseAzureController.go
+++ controllers/databaseAzureController.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/Azure/azure-sdk-for-go/services/postgresql/mgmt/2021-06-01/postgresqlflexibleservers"
 	"github.com/Azure/go-autorest/autorest/azure/auth"
+	"github.com/Azure/go-autorest/autorest/to"
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
@@ -48,5 +49,106 @@ func GetDatabaseAzureHandler(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK,dbListResult.NotDone())
+
+}
+
+func CreateDatabaseAzureHandler(c *gin.Context) {
+
+	ctx:=context.Background()
+
+	subscriptionID := "15608984-3c5b-41dc-9e79-5b17be37947a"
+	tenantID := "9b4786c5-38d8-4442-b63f-d2c8d41d0e95"
+	clientID := "e2c85ab7-e254-43da-ad29-a4799f45f4fc"
+	clientSecret := "f_A8Q~GYHeMSmJ-yImc_roVeVseNZ-zoe1I5KdBd"
+
+	config := auth.NewClientCredentialsConfig(clientID,clientSecret,tenantID)
+
+	authorizer,err := config.Authorizer()
+	if err != nil{
+		log.Fatal("Authorization hatası ,",err)
+	}
+	if err == nil{
+		fmt.Println("err şu anda boş gardaşım")
+	}
+	resourceGroup:="bitirme"
+	location:="eastus"
+	serverName:="postgresqlemirhanbitirme"
+
+	// PostgreSQL esnek sunucu istemci oluşturma
+	psqlClient := postgresqlflexibleservers.NewServersClient(subscriptionID)
+	psqlClient.Authorizer = authorizer
+
+	// PostgreSQL sunucusu için yapılandırma ayarları
+	serverProperties := postgresqlflexibleservers.ServerProperties{
+		AdministratorLogin:         to.StringPtr("emirhan"),
+		AdministratorLoginPassword: to.StringPtr("dogandemir"),
+		Version: postgresqlflexibleservers.ServerVersion("11"),
+		Storage: &postgresqlflexibleservers.Storage{
+			StorageSizeGB: to.Int32Ptr(128),
+		},
+	}
+
+	future, err := psqlClient.Create(ctx, resourceGroup, serverName, postgresqlflexibleservers.Server{
+		Location: to.StringPtr(location),
+		ServerProperties: &serverProperties,
+		Sku: &postgresqlflexibleservers.Sku{
+			Name: to.StringPtr("Standard_D2s_v3"),
+			Tier: postgresqlflexibleservers.SkuTierGeneralPurpose,
+		},
+	})
+	if err != nil {
+		log.Fatalf("Failed to start create server operation: %s", err)
+	}
+	if err ==nil{
+		fmt.Println("Future : ", future)
+	}
+
+	err = future.WaitForCompletionRef(context.Background(), psqlClient.Client)
+	if err != nil {
+		log.Fatalf("Failed to finish create server operation: %s", err)
+	}
+
+	fmt.Printf("Successfully created PostgreSQL server: %s", serverName)
+
+	c.JSON(http.StatusOK,"Successfull created Postgresql Server")
+
+}
+
+func DeleteDatabaseAzureHandler(c *gin.Context) {
+
+	ctx:=context.Background()
+
+	subscriptionID := "15608984-3c5b-41dc-9e79-5b17be37947a"
+	tenantID := "9b4786c5-38d8-4442-b63f-d2c8d41d0e95"
+	clientID := "e2c85ab7-e254-43da-ad29-a4799f45f4fc"
+	clientSecret := "f_A8Q~GYHeMSmJ-yImc_roVeVseNZ-zoe1I5KdBd"
+
+	config := auth.NewClientCredentialsConfig(clientID,clientSecret,tenantID)
+
+	authorizer,err := config.Authorizer()
+	if err != nil{
+		log.Fatal("Authorization hatası ,",err)
+	}
+	if err == nil{
+		fmt.Println("err şu anda boş gardaşım")
+	}
+	resourceGroupName := "bitirme"
+	serverName := "postgresqlemirhanbitirme"
+
+
+	psqlClient := postgresqlflexibleservers.NewServersClient(subscriptionID)
+	psqlClient.Authorizer = authorizer
+
+	future, err := psqlClient.Delete(ctx, resourceGroupName, serverName)
+	if err != nil {
+		log.Fatalf("Failed to start delete server operation: %v", err)
+	}
+
+	err = future.WaitForCompletionRef(ctx, psqlClient.Client)
+	if err != nil {
+		log.Fatalf("Failed to finish delete server operation: %v", err)
+	}
+
+	fmt.Printf("Successfully deleted PostgreSQL server: %s", serverName)
 
 }

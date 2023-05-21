0bee0a2edbe8f636dc6a4a29ef3286ccbd96fc29
Emirhan Dogandemir <emirhandgndmr51@gmail.com>
2023-05-21 15:03:51 +0300

    databaseAws model has been created

--- models/databaseAws.go
+++ models/databaseAws.go
@@ -1,1 +1,10 @@
 package models
+
+type DatabaseAws struct {
+	DbInstanceIdentifier string `json:"dbInstanceIdentifier"`
+	DbInstanceClass string `json:"dbInstanceClass"`
+	Engine string `json:"engine"`
+	MasterUsername string `json:"masterUsername"`
+	MasterPassword string `json:"masterPassword"`
+	DbName string `json:"DbName"`
+}

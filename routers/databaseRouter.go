06564ce3d238ece13f312195a569dd5b3a410738
Emirhan Dogandemir <emirhandgndmr51@gmail.com>
2023-05-21 15:16:01 +0300

    have been created databaseRouter

--- routers/databaseRouter.go
+++ routers/databaseRouter.go
@@ -1,3 +1,15 @@
 package routers
 
+import (
+	"github.com/emirhandogandemir/bitirmego/cloud-infra-rest1/controllers"
+	"github.com/gin-gonic/gin"
+)
 
+func SetupDatabaseRoutes(router *gin.Engine) {
+	databaseGroup := router.Group("/database")
+	{
+		databaseGroup.GET("/getdatabaseaws",controllers.GetDatabaseAwsHandler)
+		databaseGroup.POST("/createdatabaseaws",controllers.CreateDatabaseAwsHandler)
+		databaseGroup.POST("/deletedatabaseaws",controllers.DeleteDatabaseAwsHandler)
+	}
+}

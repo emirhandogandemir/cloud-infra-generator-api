f9a71f2cc793fc62f7eb464b620ce39299886d0c
Emirhan Dogandemir <emirhandgndmr51@gmail.com>
2023-05-22 21:23:55 +0300

    createUser and GetUserById method has been added to UserRepository

--- repositories/userRepository.go
+++ repositories/userRepository.go
@@ -1,1 +1,23 @@
 package repositories
+
+import (
+	"github.com/emirhandogandemir/bitirmego/cloud-infra-rest1/models"
+	"gorm.io/gorm"
+)
+
+type UserRepository struct {
+	DB *gorm.DB
+}
+
+func (r *UserRepository) CreateUser (user *models.User)error{
+	return r.DB.Create(user).Error
+}
+
+func(r *UserRepository) GetUserById(userId int64)(*models.User,error){
+	user:= &models.User{}
+	result := r.DB.First(user,userId)
+	if result.Error != nil{
+		return nil,result.Error
+	}
+	return user,nil
+}

97cd998274bb9449d68eaf64aa99d14aeb9f588c
Emirhan Dogandemir <emirhandgndmr51@gmail.com>
2023-06-02 20:52:53 +0300

    name field added to aws-azure(AccessModel)

--- models/awsAccessModel.go
+++ models/awsAccessModel.go
@@ -5,6 +5,7 @@ import "gorm.io/gorm"
 type AwsAccessModel struct {
 	gorm.Model
 	UserID uint `json:"user_id"`
+	Name string `json:"name"`
 	AccessKey string `json:"accessKey"`
 	SecretKey string `json:"secretKey"`
 }
--- models/azureAccessModel.go
+++ models/azureAccessModel.go
@@ -5,6 +5,7 @@ import "gorm.io/gorm"
 type AzureAccessModel struct {
 	gorm.Model
 	UserID uint `json:"user_id"`
+	Name string `json:"name"`
 	SubscriptionId string `json:"subscriptionId"`
 	TenantId string `json:"tenantId"`
 	ClientID string `json:"clientID"`

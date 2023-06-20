23b7ba5a2809a4f2538877cabf49c17e424aa379
Emirhan Dogandemir <emirhandgndmr51@gmail.com>
2023-06-20 09:58:31 +0300

    cors middleware has been added

--- main.go
+++ main.go
@@ -3,22 +3,14 @@ package main
 import (
 	"fmt"
 	"github.com/emirhandogandemir/bitirmego/cloud-infra-rest1/controllers"
+	"github.com/emirhandogandemir/bitirmego/cloud-infra-rest1/middleware/cors"
 	"github.com/emirhandogandemir/bitirmego/cloud-infra-rest1/routers"
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
-
-	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"http://localhost:3000"} // frontend'inizin çalıştığı adresi buraya ekleyin.
-	config.AllowCredentials = true
-	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"}
-	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type"}
-
-	router.Use(cors.New(config))
-
+	router.Use(cors.CORSMiddleware())
 	router.GET("/getinstancetypesaws",controllers.GetInstanceTypeHandler)
 	routers.SetupUserRoutes(router)
 	routers.SetupVirtualMachinesRoutes(router)

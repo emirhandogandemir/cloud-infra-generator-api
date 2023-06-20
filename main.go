782c95db8500c3853a47803da0358bd14f8454d9
Emirhan Dogandemir <emirhandgndmr51@gmail.com>
2023-06-20 09:42:50 +0300

    cors implementation has been added

--- main.go
+++ main.go
@@ -4,11 +4,21 @@ import (
 	"fmt"
 	"github.com/emirhandogandemir/bitirmego/cloud-infra-rest1/controllers"
 	"github.com/emirhandogandemir/bitirmego/cloud-infra-rest1/routers"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:3000"} // frontend'inizin çalıştığı adresi buraya ekleyin.
+	config.AllowCredentials = true
+	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"}
+	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type"}
+
+	router.Use(cors.New(config))
+
 	router.GET("/getinstancetypesaws",controllers.GetInstanceTypeHandler)
 	routers.SetupUserRoutes(router)
 	routers.SetupVirtualMachinesRoutes(router)
